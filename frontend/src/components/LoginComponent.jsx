27f91f7665b74dc4e1a06f09fb7dea30dc75a8c7
pravboi <pravachanpatra@gmail.com>
2024-10-24 06:06:40 -0500

    login/register errors

--- frontend/src/components/LoginComponent.jsx
+++ frontend/src/components/LoginComponent.jsx
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+
 
 const LoginComponent = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/login", {email, password}) // send post-request to this url passing form-data, this url port is specified in index.js listen()
+    .then(result => {  // if request is succesful then route them to login page
+      console.log(result);
+      if (result.data === "succesfully logged in user") { // check response, then navigate to home
+        navigate("/");
+      }
+      
+    })
+    .catch(err => {
+      if (err.response && err.response.status === 400) {
+        setError("Incorrect email/password or user doesn't exist");
+      } else {
+        setError("An error occurred, please try again.");
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <form onSubmit={LoginComponent}>
+      <form onSubmit={handleSubmit}>
+
         <h1 style={{ color: 'black',fontWeight: 'bold',fontSize:20 }}>Login</h1>
+        
+        {error && <p style={{ color: 'blue' }}>{error}</p>}
       
         
         <label style={{ color: 'black' }}>Email</label>
-        <input type="email" placeholder="enter email" />
+        <input type="email" placeholder="enter email" onChange={(e) => setEmail(e.target.value)}/>
+        
         
         <label style={{ color: 'black' }}>Password</label>
-        <input type="password" placeholder="enter password" />
+        <input type="password" placeholder="enter password" onChange={(e) => setPassword(e.target.value)}/>
         
-        <button type="submit" style={{ color: 'black',fontWeight: 'bold', }}>Sign In</button>
-        <p className= "text-sm text-center mt-4"> Not register yet?{" "}<Link to = "/Register" className=''> Create an Account </Link> 
-            </p>
+        
+        <button type="submit" style={{ color: 'black',fontWeight: 'bold', }}>Login In</button>
+        
+        
+        <p className= "text-sm text-center mt-4"> Don't have an account?{" "}<Link to = "/Register" className=''> Create an Account </Link> </p>
       </form>
   
       
--- frontend/src/components/Registercomponet.jsx
+++ frontend/src/components/Registercomponet.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RegisterComponent.css';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
 
 
 const RegisterComponent = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  console.log("cur-name: "+name);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/register", {name, email, password}) // send post-request to this url passing form-data, this url port is specified in index.js listen()
+    .then(result => {  // if request is succesful then route them to login page
+      console.log(result);
+      navigate("/login");
+
+    })
+    .catch(err => {
+      if (err.response && err.response.status === 400) {
+        setError("Email already exists, please use a different email.");
+      } else {
+        setError("An error occurred, please try again.");
+      }
+    });
+  }
+
   return (
-    <div className="container"> {/* Use the container class for centering */}
-      <form onSubmit={RegisterComponent}>
-      <h1 style={{ color: 'black', fontWeight: 'bold',fontSize:20 }}>Register</h1>
-      <label style={{ color: 'black' }}>Name</label>
-        <input type="text" placeholder="enter name" />
+    <div className="container"> 
+      <form onSubmit={handleSubmit}>
+    
+        <h1 style={{ color: 'black', fontWeight: 'bold',fontSize:20 }}>Register</h1>
+        {error && <p style={{ color: 'blue' }}>{error}</p>}
+
+        
+        <label style={{ color: 'black' }}>Name</label>
+        <input type="text" placeholder="enter name" onChange={(e) => setName(e.target.value)} />
+        
         
         <label style={{ color: 'black' }}>Email</label>
-        <input type="email" placeholder="enter email" />
+        <input type="email" placeholder="enter email" onChange={(e) => setEmail(e.target.value)}/>
+        
         
         <label style={{ color: 'black' }}>Password</label>
-        <input type="password" placeholder="enter password" />
+        <input type="password" placeholder="enter password" onChange={(e) => setPassword(e.target.value)}/>
         
         
+        {/* Submmit button */}
         <button type="create" style={{ color: 'black', fontWeight: 'bold'}}>Create Account</button>
-        <p className= "text-sm text-center mt-4"> Have account?{" "}<Link to = "/login" className=''> LOGIN </Link> 
-            </p>
+        
+
+
+
+
+        <p className= "text-sm text-center mt-4"> Have account?{" "}<Link to = "/login" className=''> LOGIN </Link> </p>
       </form>
     </div>
   );
